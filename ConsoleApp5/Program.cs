b9cd659924a4a5884e5baf36a9d668956109b582
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-02-10 10:47:13 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = "Zdarova bro, uletau na Gaity";
+            int lowercaseCount = 0;
+            foreach (char c in content)
+            {
+                if (char.IsLower(c))
+                {
+                    lowercaseCount++;
+                }
+            }
+            Console.WriteLine($"Количество строчных букв: {lowercaseCount}");
+            Console.ReadKey();
+        }
+    }
+
+}
+
+
